40f67105e2c8ef71b9502e9c0889819f60cddda7
harsh-not-haarsh <anand.2@iitj.ac.in>
2020-11-03 21:12:34 +0530

    on fail forward

--- assets/js/contact.js
+++ assets/js/contact.js
@@ -58,24 +58,22 @@ $(document).ready(function () {
                 submitHandler: function (form) {
                     $.ajax({
                         url: "https://docs.google.com/forms/d/e/1FAIpQLSfA5gdAjLa9wAKWW_6LU8sj0EdsGRtEdMIrEY7tJFPssm4aBw/formResponse",
-
-                        data: {
-                            "entry.1022086369": $('#name').val(),
-                            "entry.549682075": $('#email').val(),
-                            "entry.886790186": $('#info').val()
-                        },
+                        data: $(form).serialize(),
+                        async: false,
+                        cache: false,
                         type: "POST",
                         dataType: "xml",
                         crossDomain: true,
 
                         success: function(data){
-                        //   window.location.replace("youraddress");
+                          window.location.replace("http://ared.co.in/");
                         },
                         error: function(data){
-                            console.log();
+                            window.location.replace("http://ared.co.in/");
+                            
                         }
+
                     });
-                    window.location.href = "http://ared.co.in";
                 }
             })
         })
